281b98c008cb6fc0411905fa3d553b90b3862535
“pyqpyqpyq” <alwayswannaflyx@gmail.com>
2020-11-07 11:18:11 +0800

    [pyq]: finish task1 of adding and deleting student

---
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/controller/StudentController.java
@@ -0,0 +1,25 @@
+package com.thoughtworks.capability.gtb.restfulapidesign.controller;
+
+import com.thoughtworks.capability.gtb.restfulapidesign.entity.Student;
+import com.thoughtworks.capability.gtb.restfulapidesign.service.StudentService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    private StudentService studentService;
+    public  StudentController(StudentService studentService){
+        this.studentService=studentService;
+    }
+
+    @PostMapping
+    public void addStudent(@RequestBody Student student){
+        studentService.addStudent(student);
+    }
+
+    @DeleteMapping
+    public void deleteStudent(@RequestBody Student student){
+        studentService.deleteStudent(student);
+    }
+
+}
